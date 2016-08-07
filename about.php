ab2de931bf03a4109ade8069cf7d4faa20e50780
JGravesJr <jgravesjr@outlook.com>
2016-08-07 18:47:59 -0400

    Basic scaffold setup

--- about.php
+++ about.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Roux Academy Conference 2013 -- Home</title>	
+		<title>Roux Academy Conference 2013 -- About</title>	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 		<link href="_/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="_/css/mystyles.min.css" rel="stylesheet" media="screen">
 	</head>
-	<body id="home">
+	<body id="about">
 		<section class="container">
 			<div class="content row">
 				<section class="main col col-lg-8">
-				</section>
+				</section><!--Main-->
 				<section class="sidebar col col-lg-4">
-				<section>
+				<section><!--Sidebar-->
 			</div>
 		</section>
 		<script src="_/js/bootstrap.min.js"></script>
--- venuetravel.php
+++ venuetravel.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Roux Academy Conference 2013 -- Home</title>	
+		<title>Roux Academy Conference 2013 -- Venue/Travel</title>	
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
 		<link href="_/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="_/css/mystyles.min.css" rel="stylesheet" media="screen">
 	</head>
-	<body id="home">
+	<body id="venue">
 		<section class="container">
 			<div class="content row">
 				<section class="main col col-lg-8">
